d15da04a00f95ec825c10efab604590afe96ae96
Vicky Gupta <vicky.k.g@thecodebucket.com>
2024-11-11 17:28:13 +0530

    prime number in a range solved

--- basics/PrimeRange.java
+++ basics/PrimeRange.java
@@ -8,5 +8,40 @@ public class PrimeRange {
         System.out.print("Enter the last number: ");
         int last = scanner.nextInt();
         System.out.println("Prime numbers between " + first + " and " + last + ":");
+        for(int i=first; i<=last; i++){
+            if(i<2){
+                continue;
+               
+            }
+            else{
+                if(isPrime(i)){
+                    System.out.print(i + " ");
+                }
+            }
+            
+        }
     }
+    static boolean isPrime(int num) {
+        // if(num<2){
+        //     return false;
+        // }
+        if(num == 2){
+            return true;
+        }
+        else {
+            boolean isPrime = true;
+            for(int i = 2; i <= Math.sqrt(num); i++){
+                if(num % i == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        }
 }
