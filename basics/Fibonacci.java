109fef1431ff37d219719b9692ef6bb8d00212ab
Vicky Gupta <vicky.k.g@thecodebucket.com>
2024-11-11 18:25:00 +0530

    fibonacci number program

---
+++ basics/Fibonacci.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = in.nextInt();
+        in.close();
+        
+        long first = 0, second = 1;
+        System.out.print("Fibonacci sequence up to " + num + ": ");
+        
+        if (num >= 0) {
+            System.out.print(first + " " +second+ " ");
+            
+            for (int i = 2; i < num; i++) {
+                long next = first + second;
+                System.out.print(next + " ");
+                first = second;
+                second = next;
+            }
+        }
+        else{
+            System.out.println("Invalid input. Please enter a positive integer.");
+        }
+        System.out.println();
+
+    }
+}
