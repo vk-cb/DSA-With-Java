de824994516c9c3ee5d90c54f78d0eed08553b8a
Vicky Gupta <vicky.k.g@thecodebucket.com>
2024-11-12 17:58:31 +0530

    HCF of two numbers

--- Maths/HCF.java
+++ Maths/HCF.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class HCF{
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = in.nextInt();
+        in.close();
         
+        int ans = hcf(num1, num2);
+        System.out.print("HCF of " +num1+" and "+ num2+ " is :"+ans);
+    }
+    
+    public static int hcf(int a, int b){
+        if(a==0){
+            return b;
+        }
+        else if(b==0){
+            return a;
+        }
+        else if(a==b){
+            return a;
+        }
+        else if(a>b){
+            return hcf(a%b, b);
+        }
+        return hcf(b%a, a);
     }
 }
