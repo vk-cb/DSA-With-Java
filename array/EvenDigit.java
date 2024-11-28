21e21a3d566dfcd90d960f2d991761ef62f12560
Vicky Gupta <vicky.k.g@thecodebucket.com>
2024-11-28 14:00:08 +0530

    count even digit number in array

---
+++ array/EvenDigit.java
@@ -0,0 +1,20 @@
+public class EvenDigit {
+    public static void main(String[] args) {
+        int [] arr = {123,43,7654,9876,765};
+        int result = 0;
+        for (int i = 0; i<arr.length; i++){
+            if(isEven(arr[i])){
+                result += 1;
+            }
+        } 
+        System.out.println("Number of even digits: "+result);  
+    }
+    static boolean isEven(int num){
+        int count=0;
+        while(num>0){
+            count++;
+            num /= 10;
+        }
+        return count % 2 == 0;
+    }
+}
