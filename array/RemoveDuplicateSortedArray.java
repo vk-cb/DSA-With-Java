aa21aca07c19afd335be46137dc6517c10e89e42
Vicky Kumar Gupta <vickygupta031@gmail.com>
2024-11-30 22:42:43 +0530

    fix some bugs

--- array/RemoveDuplicateSortedArray.java
+++ array/RemoveDuplicateSortedArray.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class RemoveDuplicateSortedArray {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the length of the array: ");
--- array/SecondLargestmethod.java
+++ array/SecondLargestmethod.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class SecondLargestmethod {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
