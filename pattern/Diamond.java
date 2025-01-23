0aff7105cf0bc4ad32a36887797358303ad70b77
Vicky Kumar Gupta <vickygupta031@gmail.com>
2025-01-23 22:49:50 +0530

    fix the diamond pattern

--- pattern/Diamond.java
+++ pattern/Diamond.java
@@ -1,9 +1,10 @@
 public class Diamond {
     public static void main(String[] args) {
-        PatternDiamond(3);
+        PatternDiamond(4);
     }
 
     static void PatternDiamond(int row) {
+        System.out.println();
         for (int i = 1; i <= row * 2 - 1; i++) {
             // spaces
             int spaces = i <= row ? row - i : i - row;
@@ -11,9 +12,9 @@ public class Diamond {
                 System.out.print(" ");
             }
             // stars
-            int stars = i <= row ? 2 * i - 1 : 2 * (row * 2 - i) - 1;
+            int stars = i <= row ? i : 2*row-i;
             for (int j = 1; j <= stars; j++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
