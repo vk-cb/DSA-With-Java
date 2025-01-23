df0f45121bb60bc3417601a409c2212fffbb35c2
Vicky Kumar Gupta <vickygupta031@gmail.com>
2025-01-23 23:14:58 +0530

    pattern 8

--- pattern/Pattern3.java
+++ pattern/Pattern3.java
@@ -1,6 +1,6 @@
 public class Pattern3 {
     public static void main(String[] args) {
-        PatternFunction7(4);
+        PatternFunction8(4);
     }
     static void PatternFunction3(int row){
        
@@ -58,6 +58,20 @@ public class Pattern3 {
             }
            // print star
             for (int j = 1; j <=row-i+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    static void PatternFunction8(int row){
+     
+        for (int i = 1; i <=row; i++) {
+           // print space
+            for (int j = 1; j <=row-i; j++) {
+                System.out.print(" ");
+            }
+           // print star
+            for (int j = 1; j <=i*2-1; j++) {
                 System.out.print("*");
             }
             System.out.println();
