d2b9ea1a4907ee0051c9e34a1fef18e6b3aab4ee
Vicky Kumar Gupta <vickygupta031@gmail.com>
2025-01-23 22:57:30 +0530

    pattern 3

---
+++ pattern/Pattern3.java
@@ -0,0 +1,26 @@
+public class Pattern3 {
+    public static void main(String[] args) {
+        PatternFunction3(4);
+    }
+    static void PatternFunction3(int row){
+       
+        for (int i = 1; i <=row; i++) {
+            //formula => row-i+1;
+            for (int j = 1; j <= row-i+1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    static void PatternFunction4(int row){
+       
+        for (int i = 1; i <=row; i++) {
+            //formula => j<row;
+            // 
+            for (int j = 1; j < row; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
